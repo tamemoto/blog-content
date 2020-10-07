434f51f53e859df55906743d9fe9a03fc11d9050
tamemotoyusuke <tamemotoyou@gmail.com>
2020-10-07 12:51:04 +0900

    update

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -2,11 +2,11 @@
   <v-container fluid>
     <v-col
       v-for="item in products"
-      :key="item.product.id"
+      :key="item.id"
       cols="12"
       class="mb-5"
     >
-      <a :href="item.product.url" target="_blank" class="block p-card">
+      <a :href="item.url" target="_blank" class="block p-card">
         <v-card
           outlined
           class="mx-auto"
@@ -15,31 +15,54 @@
           <div class="p-card__item">
             <div class="p-card__image">
               <v-img
-                :src="item.product.image"
+                :src="item.image"
                 :aspect-ratio="16/9"
                 contain
-                width="90%"
+                width="100%"
                 class="ma-auto py-5 p-card__image"
               />
             </div>
             <div class="p-card__body">
+              <v-card-text class="pb-0">
+                <div class="row mt-n12 mb-6 no-gutters justify-end">
+                  <v-chip
+                    v-if="item.label === 'DEV'"
+                    small
+                    color="primary"
+                    class="font-weight-bold"
+                  >
+                    {{ item.label }}
+                  </v-chip>
+                  <v-chip
+                    v-if="item.label === 'UI'"
+                    small
+                    color="red"
+                    text-color="white"
+                    class="font-weight-bold"
+                  >
+                    {{ item.label }}
+                  </v-chip>
+                </div>
+                <div class="row group-default no-gutters">
+                  <v-chip
+                    v-for="tool in item.tools"
+                    :key="tool.index"
+                    small
+                    label
+                    dark
+                    class="mr-2 mb-2"
+                  >
+                    {{ tool }}
+                  </v-chip>
+                </div>
+              </v-card-text>
               <v-card-title>
                 <h3>
-                  {{ item.product.title }}
+                  {{ item.title }}
                 </h3>
               </v-card-title>
               <v-card-text class="p-card__text">
-                {{ item.product.text }}
-              </v-card-text>
-              <v-card-text>
-                <v-chip
-                  small
-                  dark
-                  label
-                  class="font-weight-bold"
-                >
-                  {{ item.product.label }}
-                </v-chip>
+                {{ item.text }}
               </v-card-text>
             </div>
           </div>
@@ -58,14 +81,14 @@ export default {
             meta: [
                 ...meta({
                     title: "ポートフォリオ一覧",
-                    description: "今まで制作したポートフォリオ",
+                    description: "今まで制作した制作物",
                     type: "article"
                 })
             ]
         }
     },
     async asyncData({ $content }) {
-        const products = await $content("products").fetch()
+        const { products } = await $content("products/products").fetch()
         return {
             products
         }
