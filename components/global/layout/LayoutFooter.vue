ec68a6177b67f34b428ad1e63fae0eddac847520
tamemotoyusuke <tamemotoyou@gmail.com>
2020-10-28 21:14:47 +0900

    update

--- components/global/layout/LayoutFooter.vue
+++ components/global/layout/LayoutFooter.vue
@@ -1,10 +1,15 @@
 <template>
   <v-footer class="l-footer">
     <v-container fluid>
-      <div class="l-footer__container px-8 py-6">
-        <p v-if="policy" class="mb-0">
+      <div class="l-footer__container px-8 pt-6">
+        <p v-if="policy">
           {{ policy.detail }}
         </p>
+        <div class="text-center">
+          <small>
+            © 2020, yusuke All rights reserved.
+          </small>
+        </div>
       </div>
     </v-container>
   </v-footer>
